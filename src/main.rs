b86d211351752dee065d3bd89a00638015d69bfa
Mouhamad Al Mounayar <mouhamad.almounayar@amadeus.com>
2025-07-27 22:37:55 +0200

    feat: add snippet-list component
    Signed-off-by: Mouhamad Al Mounayar <mouhamad.almounayar@amadeus.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
+use log::error;
+use simplelog::{Config, WriteLogger};
+use std::fs::File;
+
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Style, Stylize},
     text::Line,
-    widgets::{ListState, Widget},
+    widgets::{Block, Borders, List, ListItem, ListState, StatefulWidget},
 };
 
 fn main() {
+    setup_logger();
     let mut terminal = ratatui::init();
     let mut app = App::new();
     app.run(&mut terminal);
     ratatui::restore();
+}
+
+fn setup_logger() {
+    let log_file = File::create("dial.log").unwrap();
+    let result = WriteLogger::init(log::LevelFilter::Debug, Config::default(), log_file);
+    match result {
+        Ok(()) => {}
+        Err(_) => {
+            error!("There was an error initializing the logger")
+        }
+    }
 }
 
 struct Snippet {
@@ -17,6 +37,7 @@ struct Snippet {
     title: String,
 }
 
+#[derive(PartialEq)]
 enum AppMode {
     Select,
     Search,
@@ -64,14 +85,43 @@ impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) {
         while self.app_state.should_exit != true {
             let result = terminal.draw(|f: &mut Frame| {
-                self.view_manager
-                    .snippet_list_component
-                    .render(f, &self.app_state);
+                let chunks = Layout::new(
+                    Direction::Horizontal,
+                    vec![Constraint::Percentage(30), Constraint::Percentage(70)],
+                )
+                .split(f.area());
+                self.view_manager.snippet_list_component.render(
+                    chunks[0],
+                    f.buffer_mut(),
+                    &self.app_state,
+                );
             });
             match result {
                 Ok(_) => {}
                 Err(e) => {
-                    eprintln!("There was an error while calling draw {})", e)
+                    error!("There was an error while calling draw {})", e)
+                }
+            }
+
+            let result = event::read();
+            match result {
+                Ok(event) => match event {
+                    Event::Key(key) => match key.code {
+                        KeyCode::Esc => {
+                            self.app_state.should_exit = true;
+                        }
+                        _ => {
+                            if self.app_state.mode == AppMode::Select {
+                                self.view_manager
+                                    .snippet_list_component
+                                    .handle_event(&event, &mut self.app_state)
+                            }
+                        }
+                    },
+                    _ => {}
+                },
+                Err(_) => {
+                    error!("There was an error trying to read events")
                 }
             }
         }
@@ -79,26 +129,89 @@ impl App {
 }
 
 trait Component {
-    fn render(&mut self, frame: &mut Frame, state: &AppState);
+    fn render(&mut self, area: Rect, buff: &mut Buffer, state: &AppState);
+    fn handle_event(&mut self, event: &Event, state: &mut AppState);
 }
 
 struct SnippetListComponent {
-    state: ListState,
+    local_state: ListState,
 }
 
 impl SnippetListComponent {
     fn new() -> Self {
         SnippetListComponent {
-            state: ListState::default(),
+            local_state: ListState::default(),
         }
     }
 }
 
 impl Component for SnippetListComponent {
-    // placeholder render method
-    fn render(&mut self, frame: &mut Frame, state: &AppState) {
-        let widget = Line::from("Hello World");
-        widget.render(frame.area(), frame.buffer_mut());
+    fn render(&mut self, area: Rect, buff: &mut Buffer, state: &AppState) {
+        let index = state.selected_index;
+        self.local_state.select(Some(index));
+        let items = state
+            .snippet_list
+            .iter()
+            .map(|snippet| ListItem::from(snippet));
+        let block = Block::new()
+            .borders(Borders::all())
+            .title(" Snippets ")
+            .title_alignment(ratatui::layout::Alignment::Center);
+        let list = List::new(items)
+            .block(block)
+            .highlight_symbol(" > ")
+            .highlight_style(Style::default().green());
+        StatefulWidget::render(list, area, buff, &mut self.local_state);
+    }
+
+    fn handle_event(&mut self, event: &Event, state: &mut AppState) {
+        match event {
+            Event::Key(key) => match key.code {
+                KeyCode::Char('j') => {
+                    if key.kind == KeyEventKind::Press {
+                        self.select_next(state);
+                    }
+                }
+                KeyCode::Char('k') => {
+                    if key.kind == KeyEventKind::Press {
+                        self.select_previous(state);
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+impl SnippetListComponent {
+    fn select_next(&self, state: &mut AppState) {
+        let length = state.snippet_list.len();
+        if length == 0 {
+            return;
+        }
+        let index = state.selected_index + 1;
+        state.selected_index = index % length;
+    }
+
+    fn select_previous(&self, state: &mut AppState) {
+        let length = state.snippet_list.len();
+        if length == 0 {
+            return;
+        }
+        if state.selected_index == 0 {
+            state.selected_index = length - 1;
+        } else {
+            state.selected_index -= 1;
+        }
+    }
+}
+
+impl From<&Snippet> for ListItem<'_> {
+    fn from(value: &Snippet) -> Self {
+        let title = Line::from(value.title.clone()).bold();
+        let language = Line::from(value.language.clone()).italic();
+        ListItem::new(vec![title, language, Line::from("")])
     }
 }
 
